d9b00c02e462bbc8b7628547fc2428b992228fd9
Uy Tieu <utieu@netflix.com>
2018-12-29 17:53:01 -0800

    animations and components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/Card';
 
 const IndexPage = () => (
   <div>
@@ -31,6 +32,27 @@ const IndexPage = () => (
         </svg>
       </div>
     </div> 
+    <div className="Cards">
+      <h2>11 Courses, more coming</h2>
+      <div className="CardGroup">
+        <Card 
+          title="Design Systems"
+          text="10 Sections"
+          image={require('../images/wallpaper.jpg')} />
+        <Card 
+          title="React for Designers"
+          text="12 Sections"
+          image={require('../images/wallpaper2.jpg')} />
+        <Card 
+          title="Sound Design"
+          text="5 Sections"
+          image={require('../images/wallpaper3.jpg')} />
+        <Card 
+          title="ARKit 2"
+          text="10 Sections"
+          image={require('../images/wallpaper4.jpg')} />
+      </div>
+    </div>
   </div>
 )
 
