48813c5909182bd0845e9962d22793381e045786
Uy Tieu <utieu@netflix.com>
2018-12-31 10:03:30 -0800

    added section component

---
+++ src/components/Section.js
@@ -0,0 +1,38 @@
+import React from 'react' 
+import styled from 'styled-components'
+
+const SectionGroup = styled.div`
+    background: url(${props => props.image});
+    height: 720px;
+    background-size: cover;
+    display: grid;
+    grid-template-rows: 300px auto;
+    grid-gap: 20px;
+`
+
+const SectionLogo = styled.img`
+    align-self: end;
+    width: 128px;
+    margin: 0 auto;
+`
+
+const SectionTitleGroup = styled.div`
+`
+
+const SectionTitle = styled.h3`
+`
+
+const SectionText = styled.p`
+`
+
+const Section = props => (
+    <SectionGroup image={props.image}>
+        <SectionLogo src={props.logo} />
+        <SectionTitleGroup>
+            <SectionTitle>{props.title}</SectionTitle>
+            <SectionText>{props.text}</SectionText>
+        </SectionTitleGroup>
+    </SectionGroup>
+)
+
+export default Section
