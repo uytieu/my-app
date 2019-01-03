b215715cff756ff43d66dccaf967df4c7ec0e135
Uy Tieu <utieu@netflix.com>
2019-01-02 16:34:23 -0800

    json file

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,5 +1,6 @@
 import React from 'react' 
 import styled from 'styled-components'
+import Wave from './Wave';
 
 const SectionGroup = styled.div`
     background: url(${props => props.image});
@@ -8,6 +9,11 @@ const SectionGroup = styled.div`
     display: grid;
     grid-template-rows: 300px auto;
     grid-gap: 20px;
+    position: relative;
+
+    @media (max-width: 640px) {
+        height: 820px;
+    }
 `
 
 const SectionLogo = styled.img`
@@ -17,16 +23,49 @@ const SectionLogo = styled.img`
 `
 
 const SectionTitleGroup = styled.div`
+    display: grid;
+    grid-template-columns: 300px auto;
+    margin: 0 40px;
+    grid-gap: 20px;
+    grid-template-rows: auto 100%;
+
+    @media (max-width: 720px) {
+        grid-template-columns: 1fr;
+    }
 `
 
 const SectionTitle = styled.h3`
+    color: white;
+    font-size: 60px;
+    margin: 0;
+    line-height:1.2;
+
+    @media (max-width:720px) {
+        font-size: 40px;
+    }
 `
 
 const SectionText = styled.p`
+    color: white;
+`
+
+const WaveBottom = styled.div`
+    position: absolute;
+    width: 100%;
+    bottom: -6px;
+`
+
+const WaveTop = styled.div`
+    position: absolute;
+    width: 100%;
+    top: -6px;
+    transform: rotate(180deg);
 `
 
 const Section = props => (
     <SectionGroup image={props.image}>
+        <WaveTop><Wave /></WaveTop>
+        <WaveBottom><Wave /></WaveBottom>
         <SectionLogo src={props.logo} />
         <SectionTitleGroup>
             <SectionTitle>{props.title}</SectionTitle>
