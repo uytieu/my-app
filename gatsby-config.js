c2aad7bde6cb52068a73fd1354adfc0bf6feb2fb
Uy Tieu <utieu@netflix.com>
2019-01-04 14:55:37 -0800

    update text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: 'Design + Code for Uy',
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
+
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [ 
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: '35m9iwl0r6ji',
+        accessToken: '7ec8740fbe7ad8712c582209c771701d277c2cc36ad8ee059c304a1cbccec786'
+      }
+    }
+  ],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,18 +3,22 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Header from '../components/header'
 import './index.css'
+import Footer from '../components/Footer';
 
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: data.site.siteMetadata.description },
+        { name: 'keywords', content: data.site.siteMetadata.keywords },
       ]}
     />
     <Header />
       {children()}
+    <Footer data={data}>
+      Backgrounds made in Cinema 4D, iOS app in Swift, site in React. <a href="mailto:support@designcode.io">Email us</a> to ask anything. © 2018
+    </Footer>
   </div>
 )
 
@@ -29,6 +33,17 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+        description
+        keywords
+      }
+    }
+    allContentfulLink(sort: {fields: [createdAt], order: ASC}) {
+      edges {
+        node {
+          title
+          url
+          createdAt
+        }
       }
     }
   }
